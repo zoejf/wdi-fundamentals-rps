69696377c3fab83034747cce790850e7a7f031e9
Zoe Foss <zoejfoss@Zoes-MacBook-Pro.local>
2015-05-03 20:31:41 -0700

    3.5 Homework - added expression for getPlayerMove and getComputerMove

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -23,16 +23,18 @@ function randomPlay() {
 
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
+    var move;
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
-}
+        return move || 'getInput()';
+    }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
+    var move; 
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move || 'randomPlay';
 }
 
 function getWinner(playerMove,computerMove) {
