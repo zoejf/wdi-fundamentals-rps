71ff6ac96f4768e82c56f6b2a3534842ac3fd211
Zoe Foss <zoejfoss@Zoes-MacBook-Pro.local>
2015-05-23 21:37:30 -0700

    Unit 4 homework - created function to retur 'player' 'computer' or 'tie' as winner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -42,7 +42,40 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove = 'rock') {
+        if computerMove = 'rock' {
+            winner = 'tie';
+        };
+        else if computerMove = 'paper' {
+            winner = 'computer';
+        };
+        else if computerMove = 'scissors' {
+            winner = 'player';
+        };
+        };
+      
+    else if (playerMove = 'paper') {
+        if computerMove = 'rock' {
+            winner = 'player';
+        };
+        else if computerMove = 'paper' {
+            winner = 'tie';
+        };
+        else if computerMove = 'scissors' {
+            winner = 'computer';
+        };
+    };
+    else if (playerMove = 'scissors') {
+        if computerMove = 'rock' {
+            winner = 'computer';
+        };
+        else if computerMove = 'paper' {
+            winner = 'player';
+        };
+        else if computerMove = 'scissors' {
+            winner = 'tie';
+        };
+    };
     return winner;
 }
 
